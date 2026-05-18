6fda47f6d0fa53fba6c2770ec32e0416ee76c928
automatedoperationdevops153 <15303768440@163.com>
2026-05-18 21:43:33 +0800

    fix:修复文件名错误

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -23,7 +23,7 @@ def run_ansible_task():
             [
                 '/usr/local/bin/ansible-playbook',
                 '-i', 'inventory/hosts.ini',
-                'api_return.yml'
+                'multiple_date.yml'
             ],
             capture_output=True,
             cwd='/app/ansible',
@@ -33,7 +33,6 @@ def run_ansible_task():
     except Exception as e:
         return False, b'', str(e).encode('utf-8')
 
-
 #返回原始JSON数据
 def get_raw_monitor_data():
     """返回原始的Python列表/字典（不是Response），供Prometheus解析"""
@@ -41,21 +40,18 @@ def get_raw_monitor_data():
     if not success:
         return []
 
-    # 编码识别和解码
     stdout_encoding = chardet.detect(stdout_bytes)['encoding'] or 'utf-8'
     stdout_str = stdout_bytes.decode(stdout_encoding, errors='ignore')
 
-    # 提取JSON字符串
     json_match = re.search(r'msg": "(\[.*?])"', stdout_str)
     if not json_match:
         return []
 
-    # 解析JSON字符串为Python列表
     try:
         pure_json = json_match.group(1)
         pure_json_clean = pure_json.replace('\\', '')
         result=json.loads(pure_json_clean)
-        return result# 返回Python列表（可遍历）
+        return result
     except json.JSONDecodeError:
         return []
 
@@ -77,17 +73,15 @@ def get_monitor_data():
 
 #数据Prometheus化（供Prometheus解析）
 def data_prmetheus():
-    # 获取原始Python列表
+
     mock_host_metrics = get_raw_monitor_data()
     if not mock_host_metrics:
         return "暂无监控数据"
 
-    # 遍历数据赋值给指标
     for host in mock_host_metrics:
         hostname = host.get("hostname")
         if not hostname:
             continue  # 主机名为空则跳过
-        # 提取数值并转浮点数，加异常处理
         try:
             disk_usage = float(host.get("disk_usage", 0))
             mem_usage = float(host.get("memory_usage", 0))
@@ -96,18 +90,16 @@ def data_prmetheus():
         except ValueError:
             continue  # 数值转换失败则跳过
 
-        # 赋值给指标
         disk_gauge.labels(hostname=hostname).set(disk_usage)
         mem_gauge.labels(hostname=hostname).set(mem_usage)
         cpu_gauge.labels(hostname=hostname).set(cpu_usage)
         load_gauge.labels(hostname=hostname).set(load_1min)
 
-    # 生成Prometheus格式文本
     prom_text = generate_latest().decode("utf-8")
     return prom_text
 
 
-#新增Prometheus采集的核心接口
+#Prometheus采集的核心接口
 @app.route('/metrics')
 def prometheus_metrics():
     prom_data = data_prmetheus()
